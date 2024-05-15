a22514c7120f4ebbbb14a7634277fd572d38b4c0
Gidi Rabi <118205453+GidiRabi@users.noreply.github.com>
2024-05-15 20:02:48 +0300

    Add files via upload

--- Q6/add2PB.cpp
+++ Q6/add2PB.cpp
@@ -1,99 +1,101 @@
-#include <iostream>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <fcntl.h>
-#include <cstring>
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        cerr << "Usage: " << argv[0] << " \"full name,phone number\"" << endl;
+int main(int argc, char *argv[])
+{
+    // Check if the correct number of arguments are provided
+    if (argc != 2)
+    {
+        cerr << "Usage: " << argv[0] << " \"<name>,<phone number>\" " << endl;
         return 1;
     }
 
-    // Print length of input string before removing newline characters
-    cout << "Length of input string before removing newline characters: " << strlen(argv[1]) << endl;
+    // Open the phonebook file, create it if it doesn't exist
+    int fileDescriptor = open("phonebook.txt", O_RDWR | O_APPEND | O_CREAT, 0644);
 
-    // Remove trailing newline character if present
-    char* input = argv[1];
-    size_t input_len = strlen(input);
-    if (input_len > 0 && input[input_len - 1] == '\n') {
-        input[input_len - 1] = '\0';
-    }
-
-    // Print length of input string after removing newline characters
-    cout << "Length of input string after removing newline characters: " << strlen(input) << endl;
-
-    // Create a pipe
-    int pipefd[2];
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
+    // Check if the file was opened successfully
+    if (fileDescriptor == -1)
+    {
+        perror("open");
         return 1;
     }
 
-    // Fork a child process for awk
-    pid_t awk_pid = fork();
-
-    if (awk_pid == -1) {
-        perror("fork");
+    // Seek to the end of the file
+    off_t end = lseek(fileDescriptor, 0, SEEK_END);
+    if (end == -1)
+    {
+        perror("lseek");
         return 1;
-    } else if (awk_pid == 0) { // Child process for awk
-        // Close the read end of the pipe
-        close(pipefd[0]);
-        cout << "AWK Child process: Redirecting stdout to pipe" << endl;
+    }
 
-        // Redirect stdout to write to the pipe
-        if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
-            perror("dup2");
+    // If the file is not empty, check the last character
+    if (end != 0)
+    {
+        // Seek to the position of the last character
+        off_t lastCharPos = lseek(fileDescriptor, -1, SEEK_CUR);
+        if (lastCharPos == -1)
+        {
+            perror("lseek");
             return 1;
         }
-        close(pipefd[1]);
 
-        // Execute awk command to print the input
-        cout << "AWK Child process: Executing awk" << endl;
-        execlp("awk", "awk", "{print $0}", NULL);
-        perror("execlp awk");
-        return 1;
-    }
-
-    // Close the write end of the pipe in the parent process
-    close(pipefd[1]);
-    cout << "Parent process: Closed write end of pipe" << endl;
-
-    // Fork another child process for sh
-    pid_t sh_pid = fork();
-
-    if (sh_pid == -1) {
-        perror("fork");
-        return 1;
-    } else if (sh_pid == 0) { // Child process for sh
-        // Redirect stdin to read from the pipe
-        if (dup2(pipefd[0], STDIN_FILENO) == -1) {
-            perror("dup2");
+        // Read the last character
+        char lastChar;
+        ssize_t numRead = read(fileDescriptor, &lastChar, 1);
+        if (numRead == -1)
+        {
+            perror("read");
             return 1;
         }
-        close(pipefd[0]);
-        cout << "SH Child process: Redirecting stdin from pipe" << endl;
 
-        // Execute sh command to append to file
-        cout << "SH Child process: Executing cat and appending to phonebook.txt" << endl;
-        execlp("/bin/sh", "sh", "-c", "cat >> phonebook.txt", NULL);
-        perror("execlp sh");
-        return 1;
+        // If the last character is not a newline, append a newline
+        if (lastChar != '\n')
+        {
+            write(fileDescriptor, "\n", 1);
+        }
     }
 
-    // Close the read end of the pipe in the parent process
-    close(pipefd[0]);
-    cout << "Parent process: Closed read end of pipe" << endl;
+    // Iterate over the command line arguments
+    for (int i = 1; i < argc; i++)
+    {
+        // Create a child process
+        int pid1;
+        if ((pid1 = fork()) == 0)
+        {
+            // Redirect the output of the process to the file
+            dup2(fileDescriptor, STDOUT_FILENO);
+            close(fileDescriptor);
 
-    // Wait for both child processes to finish
-    waitpid(awk_pid, NULL, 0);
-    cout << "Parent process: Waited for AWK child process" << endl;
-    waitpid(sh_pid, NULL, 0);
-    cout << "Parent process: Waited for SH child process" << endl;
+            // Execute the echo command with the argument
+            if (i == argc - 1)
+            {
+                execlp("echo", "echo", argv[i], NULL);
+            }
+            else
+            {
+                execlp("echo", "echo", "-n", argv[i], "", NULL);
+            }
+            perror("execlp");
+            return 1;
+        }
+        else if (pid1 == -1)
+        {
+            perror("fork");
+            return 1;
+        }
+        else
+        {
+            // Wait for the child process to finish
+            waitpid(pid1, NULL, 0);
+        }
+    }
 
     return 0;
 }
-
